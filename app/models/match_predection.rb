d96b7934737cd7b4c4fa301b10bdc69f1f1eb696
Aravind <aravind.selvamani@freshworks.com>
2021-05-01 08:48:55 +0530

    fix

--- app/models/match_predection.rb
+++ app/models/match_predection.rb
@@ -2,7 +2,7 @@ class MatchPredection < ApplicationRecord
   before_save :set_date
   validates :name, presence: true
   def set_date
-    today = Time.now.in_time_zone("Chennai").strftime("%d-%b-%y")
+    today = Time.now.in_time_zone("Chennai").strftime("%-d-%b-%y")
     match_start_date = "9-Apr-21"
     self.date = Date.parse(today) >= Date.parse(match_start_date) ? today : match_start_date
   end 
